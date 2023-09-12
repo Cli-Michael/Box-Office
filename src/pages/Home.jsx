dcab34d1c24e1c7d5d2c482f856582a343542824
Michael <michaelclinton511@gmail.com>
2023-09-12 14:12:29 +0530

    displayed show results and small fetch refractor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,63 @@
-import MainPageLayout from "../components/MainPageLayout";
-import { useState } from "react";
+import MainPageLayout from '../components/MainPageLayout';
+import { useState } from 'react';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
+  // eslint-disable-next-line
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
 
-  // eslint-disable-next-line 
-  const [ input, setInput] = useState('');
-
-  const onInputChange = (evt) => {
+  const onInputChange = evt => {
     setInput(evt.target.value);
     // console.log(evt.target.value);
-  }
+  };
 
   const onSearch = () => {
     // https://api.tvmaze.com/search/shows?q=girls
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r => r.json()).then(result => {
-      console.log(result);
+ 
+    apiGet(`https://api.tvmaze.com/search/shows?q=${input}`).then(result => {
+      setResults(result);
     });
   };
 
-  const onKeyDown = (evt) => {
-    if(evt.keyCode === 13){
+  const onKeyDown = evt => {
+    if (evt.keyCode === 13) {
       onSearch();
     }
-  }
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No Results</div>;
+    }
+
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}> {item.show.name} </div>
+          ))}
+        </div>
+      );
+    }
+
+    return null;
+  };
 
   return (
     <MainPageLayout>
-      <input type="text" onChange={ onInputChange } onKeyDown={ onKeyDown } value={ input } />
-      <button type="button" onClick={ onSearch }>Search</button>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+      {renderResults()}
     </MainPageLayout>
-  )
-}
+  );
+};
 
 export default Home;
